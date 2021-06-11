1ec0c0cd7bd709caa316a6bad685f205ff68e2a1
leabs <stevenleabo@gmail.com>
2021-06-11 08:49:14 -0400

    adding click sound

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $('.popit__circle').on('click', function() {
-    $(this).toggleClass('active');
+	$(this).toggleClass('active');
+});
+
+//pop sound
+$(document).ready(function() {
+	var obj = document.createElement('audio');
+	obj.src = 'click.mp3';
+	obj.volume = 1;
+	obj.autoPlay = false;
+	obj.preLoad = true;
+
+	$('.popit__circle').click(function() {
+		obj.play();
+	});
 });
